2afb62799a8b742e8d07ea7945f0cc62cf9d700d
Maxime Bodereau <maxime.bodereau@wolterskluwer.com>
2015-01-28 11:29:37 -0500

    Mobile iphone ready

--- js/drag-it.js
+++ js/drag-it.js
@@ -6,7 +6,7 @@ $(function() {
         placeholder: "portlet-placeholder ui-corner-all",
         update: function () {
             var countingfirstcolumn = $(".userid_jean_simard .notstarted .panel-body .drakard").length;
-            $( ".userid_jean_simard .notstarted .panel-title" ).text( countingfirstcolumn + " Files" );
+            $( ".userid_jean_simard .notstarted .panel-title .numberoffiles" ).text( countingfirstcolumn + " Files" );
                 if ( countingfirstcolumn >= 4) {$('.column.notstarted .panel.panel-default').addClass( "panel-danger" );};
                 if ( countingfirstcolumn <= 4) {$('.column.notstarted .panel.panel-default').removeClass( "panel-danger" );};
             var countingscdcolumn = $(".userid_jean_simard .started .panel-body .drakard").length;
@@ -25,29 +25,6 @@ $(function() {
             $( ".userid_jean_simard .completed .panel-title" ).text( countingffthcolumn + " Files" );
                 if ( countingffthcolumn >= 4) {$('.column.completed .panel.panel-default').addClass( "panel-danger" );};
                 if ( countingffthcolumn <= 4) {$('.column.completed .panel.panel-default').removeClass( "panel-danger" );};
-
-
-            var countingfirstcolumn = $(".userid_thomas_tremblay .notstarted .panel-body .drakard").length;
-            $( ".userid_thomas_tremblay .notstarted .panel-title" ).text( countingfirstcolumn + " Files" );
-            if ( countingfirstcolumn >= 4) {$('.column.notstarted .panel.panel-default').addClass( "panel-danger" );};
-            if ( countingfirstcolumn <= 4) {$('.column.notstarted .panel.panel-default').removeClass( "panel-danger" );};
-            var countingscdcolumn = $(".userid_thomas_tremblay .started .panel-body .drakard").length;
-            $( ".userid_thomas_tremblay .started .panel-title" ).text( countingscdcolumn + " Files" );
-            if ( countingscdcolumn >= 4) {$('.column.started .panel.panel-success').addClass( "panel-danger" );};
-            if ( countingscdcolumn <= 4) {$('.column.started .panel.panel-success').removeClass( "panel-danger" );};
-            var countingthrdcolumn = $(".userid_thomas_tremblay .review .panel-body .drakard").length;
-            $( ".userid_thomas_tremblay .review .panel-title" ).text( countingthrdcolumn + " Files" );
-            if ( countingthrdcolumn >= 4) {$('.column.review .panel.panel-warning').addClass( "panel-danger" );};
-            if ( countingthrdcolumn <= 4) {$('.column.review .panel.panel-warning').removeClass( "panel-danger" );};
-            var countingfrthcolumn = $(".userid_thomas_tremblay .reviewpartner .panel-body .drakard").length;
-            $( ".userid_thomas_tremblay .reviewpartner .panel-title" ).text( countingfrthcolumn + " Files" );
-            if ( countingfrthcolumn >= 4) {$('.column.reviewpartner .panel.panel-success').addClass( "panel-danger" );};
-            if ( countingfrthcolumn <= 4) {$('.column.reviewpartner .panel.panel-success').removeClass( "panel-danger" );};
-            var countingffthcolumn = $(".userid_thomas_tremblay .completed .panel-body .drakard").length;
-            $( ".userid_thomas_tremblay .completed .panel-title" ).text( countingffthcolumn + " Files" );
-            if ( countingffthcolumn >= 4) {$('.column.completed .panel.panel-default').addClass( "panel-danger" );};
-            if ( countingffthcolumn <= 4) {$('.column.completed .panel.panel-default').removeClass( "panel-danger" );};
-
         }
     });
 
@@ -60,6 +37,19 @@ $(function() {
         var icon = $( this );
         icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
     });
+
+
+    var countingfirstcolumn = $(".userid_jean_simard .notstarted .panel-body .drakard").length;
+    $( ".userid_jean_simard .notstarted .panel-title .numberoffiles" ).text( countingfirstcolumn + " Files" );
+    var countingscdcolumn = $(".userid_jean_simard .started .panel-body .drakard").length;
+    $( ".userid_jean_simard .started .panel-title" ).text( countingscdcolumn + " Files" );
+    var countingthrdcolumn = $(".userid_jean_simard .review .panel-body .drakard").length;
+    $( ".userid_jean_simard .review .panel-title" ).text( countingthrdcolumn + " Files" );
+    var countingfrthcolumn = $(".userid_jean_simard .reviewpartner .panel-body .drakard").length;
+    $( ".userid_jean_simard .reviewpartner .panel-title" ).text( countingfrthcolumn + " Files" );
+    var countingffthcolumn = $(".userid_jean_simard .completed .panel-body .drakard").length;
+    $( ".userid_jean_simard .completed .panel-title" ).text( countingffthcolumn + " Files" );
+
 });
 
 
